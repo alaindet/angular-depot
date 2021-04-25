eedf775983c2b5cf5d081a134a7074f8ae981209
Alain D'Ettorre <alain.det@gmail.com>
2021-04-26 00:49:57 +0200

    Add responsiveness to table

--- angular/tachytable/src/app/tachytable/components/table/table.component.ts
+++ angular/tachytable/src/app/tachytable/components/table/table.component.ts
@@ -1,4 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface CssRules {
+  [rule: string]: string;
+}
 
 @Component({
   selector: 'tac-table',
@@ -6,30 +10,63 @@ import { Component, Input } from '@angular/core';
     .table {
       display: grid;
       /* TODO: Dynamic number of columns */
-      grid-template-columns: repeat(8, 1fr);
-      border-top: 1px solid #888;
-      border-right: 1px solid #888;
+      grid-template-columns: repeat(6, 1fr);
+      border-top: 1px solid #aaa;
+      border-right: 1px solid #aaa;
     }
     .table-item {
-      padding: 0.5rem 0.25rem;
-      border-left: 1px solid #888;
-      border-bottom: 1px solid #888;
+      padding: 0.5rem;
+      border-left: 1px solid #aaa;
+      border-bottom: 1px solid #aaa;
+    }
+    .even {
+      background-color: #eee;
+    }
+    .odd + .even {
+      color: blue;
+    }
+    .even + .odd {
+      color: red;
     }
     .header {
       font-weight: bold;
     }
+
+    @media only screen and (max-width: 768px) {
+      .table {
+        grid-template-columns: 1fr !important;
+        border: none;
+      }
+      .table-item {
+        border: none;
+        border-bottom: 1px solid #888;
+      }
+      .header {
+        display: none;
+      }
+      .even + .odd {
+        margin-top: 2rem;
+      }
+      .odd + .even {
+        margin-top: 2rem;
+      }
+      .cell::before {
+        content: attr(data-header);
+        font-weight: bold;
+      }
+    }
   `],
   template: `
-    <div class="table">
+    <div class="table" [ngStyle]="tableCss">
       <ng-container *ngFor="let header of headers">
         <div class="table-item header">{{ header.label }}</div>
       </ng-container>
-      <ng-container *ngFor="let row of rows; let rowIndex = index">
+      <ng-container *ngFor="let row of rows; let rowIndex = index; let even = even">
         <ng-container *ngFor="let header of headers">
           <div
-            class="table-item cell"
+            class="table-item cell {{ even ? 'even' : 'odd' }}"
             attr.data-row="{{ rowIndex }}"
-            attr.data-header="{{ header.label }}"
+            attr.data-header="{{ header.label }}: "
           >
             {{ row[header.key] }}
           </div>
@@ -38,8 +75,20 @@ import { Component, Input } from '@angular/core';
     </div>
   `,
 })
-export class TachyTableComponent {
+export class TachyTableComponent implements OnInit {
 
   @Input() headers: any = [];
   @Input() rows: any = [];
+
+  tableCss: CssRules = {};
+
+  ngOnInit(): void {
+    this.tableCss = this.buildTableCss();
+  }
+
+  private buildTableCss(): CssRules {
+    return {
+      'grid-template-columns': `repeat(${this.headers.length}, 1fr)`,
+    };
+  }
 }
